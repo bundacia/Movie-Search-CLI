ba5e0cab3b1b920d1eebeea8c44ceee109d3e952
Trevor Little <trevor@learnersguild.org>
2017-06-28 11:01:47 -0400

    adding sepia to cache imdb fixture

--- test/imdb.js
+++ test/imdb.js
@@ -1,10 +1,9 @@
+require('sepia')
 const expect = require('chai').expect
 
 const queryIMDB = require('../imdb').queryIMDB
 
 describe('imdb.js', function () {
-  this.timeout(6000)
-
   it('can find nemo!', function (done) {
     queryIMDB('nemo', (err, results) => {
       expect(err).to.be.null
