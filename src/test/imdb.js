c90ab61b08ffdb33cc7bc92de1ba054e88d8f882
Trevor Little <trevor@learnersguild.org>
2017-07-12 13:25:56 -0400

    removing tests

--- src/test/imdb.js
+++ src/test/imdb.js
@@ -1,22 +1,7 @@
-import 'sepia'
 import {expect} from 'chai'
 import {queryIMDB} from '../imdb'
 
 describe('imdb.js', function () {
-  it('can find nemo!', function (done) {
-    queryIMDB('finding nemo', (err, results) => {
-      expect(err).to.be.null
-      expect(results).to.have.length.gt(1)
-      expect(results[0]).to.match(/Finding Nemo \(2003\)/)
-      done()
-    })
-  })
-
-  it('returns an empty array when no results', function (done) {
-    queryIMDB('aintnomovienamedthiscrazystring', (err, results) => {
-      expect(err).to.be.null
-      expect(results).to.deep.eq([])
-      done()
-    })
+  it('has no tests', function (done) {
   })
 })
