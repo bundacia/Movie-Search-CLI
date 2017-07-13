3ff8cc69170cb821d76e40e69b9e4ffd3ff15de5
Trevor Little <trevor@learnersguild.org>
2017-07-13 09:54:28 -0400

    replacing sepia tests with nock

--- src/test/imdb.test.js
+++ src/test/imdb.test.js
@@ -1,23 +1,47 @@
-import 'sepia'
+import nock from 'nock'
 import {expect} from 'chai'
 import {queryIMDB, parseResultsPage} from '../imdb'
 
 describe('queryIMDB', function () {
+
   it('can find nemo!', function (done) {
+    nock('http://www.imdb.com')
+      .get('/find')
+      .query(true)
+      .reply(200, `
+        <div class='findSection'>
+          <table class="findList">
+            <tr> <td class="result_text">Rocky</td> </tr>
+            <tr> <td class="result_text">Rocky II</td> </tr>
+          </table>
+        </div>
+      `)
+
     queryIMDB('finding nemo', (err, results) => {
       expect(err).to.be.null
-      expect(results).to.have.length.gt(1)
-      expect(results[0]).to.match(/Finding Nemo \(2003\)/)
+      expect(results).to.deep.eq([
+        'Rocky',
+        'Rocky II',
+      ])
       done()
     })
   })
 
   it('returns an empty array when no results', function (done) {
+    nock('http://www.imdb.com')
+      .get('/find')
+      .query(true)
+      .reply(200, '<div class="findNoResults">&nbsp;</div>')
+
     queryIMDB('aintnomovienamedthiscrazystring', (err, results) => {
       expect(err).to.be.null
       expect(results).to.deep.eq([])
       done()
     })
+  })
+
+  afterEach(function() {
+    nock.restore()
   })
 })
 
